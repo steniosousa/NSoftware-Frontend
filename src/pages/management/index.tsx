243309e879da4e8bf9fe7fa038134868c8d1b9d0
Stenio Sousa <steniosousaf@gmail.com>
2023-05-25 21:33:53 -0300

    feat: [200~

--- src/pages/management/index.tsx
+++ src/pages/management/index.tsx
@@ -3,28 +3,28 @@ import Graphic from '../../components/graphic';
 import Header from '../../components/Header';
 import axios from 'axios';
 
+type employeeProps = {
+    id:number,
+    companyCode:string,
+    password: string,
+    name:string,
+    email: string
+}
+
 const Management: React.FC = () => {
-    const [data, setData] = useState([
-        { username: 'user1', role: '', approved: false },
-        { username: 'user2', role: '', approved: false },
-        { username: 'user3', role: '', approved: false },
-      ]);
+    const [data, setData] = useState<employeeProps[]>([])
       
       async function getData(){
-        const { data } = await axios.get(`http://localhost:3000/employee?companyCod=435F57X`);
-        console.log(data)
+        const { data } = await axios.get('http://localhost:3000/employee', {params: {companyCode: '435F57X'}})
+        setData(data)
       }
     
-      const handleRoleChange = (index:any, event:any) => {
-        const newData = [...data];
-        newData[index].role = event.target.value;
-        setData(newData);
+      const handleRoleChange = ( event:any) => {
+        console.log(event)
       };
     
       const handleApprovalChange = (index:any) => {
-        const newData = [...data];
-        newData[index].approved = !newData[index].approved;
-        setData(newData);
+        console.log(index)
       };
 
 useEffect(() =>{
@@ -154,39 +154,38 @@ useEffect(() =>{
                                 <thead className="bg-gray-50">
                                 <tr>
                                     <th className="p-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Nome de Usuário
+                                    Nome
                                     </th>
                                     <th className=" p-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Função
                                     </th>
                                     <th className=" p-3 px-6 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Aprovado
+                                    Exercendo
                                     </th>
                                 </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                {data.map((item, index) => (
-                                    <tr key={index}>
+                                {data.map((item) => (
+                                    <tr key={item.id}>
                                     <td className="py-4 px-6 whitespace-nowrap">
-                                        <div className="text-sm text-gray-900">{item.username}</div>
+                                        <div className="text-sm text-gray-900">{item.name}</div>
                                     </td>
                                     <td className="py-4 px-6 whitespace-nowrap">
                                         <select
-                                        className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                        value={item.role}
-                                        onChange={(e) => handleRoleChange(index, e)}
+                                            className="block w-full border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                            onChange={(e) => handleRoleChange(e.target.value)}
                                         >
                                         <option value="">Selecione</option>
-                                        <option value="Gerente">Cozinheiro</option>
+                                        <option value="Cozinheiro">Cozinheiro</option>
                                         <option value="Motoboy">Motoboy</option>
+                                        <option value="Garçom">Garçom</option>
                                         </select>
                                     </td>
                                     <td className="py-4 px-6 whitespace-nowrap">
                                         <input
                                         type="checkbox"
                                         className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                                        checked={item.approved}
-                                        onChange={() => handleApprovalChange(index)}
+                                        onChange={() => handleApprovalChange(item.id)}
                                         />
                                     </td>
                                     </tr>
@@ -208,47 +207,6 @@ useEffect(() =>{
                         </div>
                     </div>
                 </div>
-
-                    <div className="w-full md:w-1/2 xl:w-1/3 p-6">
-                        <div className="bg-white border-transparent rounded-lg shadow-xl">
-                            <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
-                                <h2 className="font-bold uppercase text-gray-600">30 funcionários</h2>
-                            </div>
-                            <div className="p-5">
-                                <table className="w-full p-5 text-gray-700">
-                                    <thead>
-                                    <tr>
-                                        <th className="text-left text-blue-900">Name</th>
-                                        <th className="text-left text-blue-900">Side</th>
-                                        <th className="text-left text-blue-900">Role</th>
-                                    </tr>
-                                    </thead>
-
-                                    <tbody>
-                                    <tr>
-                                        <td>Obi Wan Kenobi</td>
-                                        <td>Light</td>
-                                        <td>Jedi</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Greedo</td>
-                                        <td>South</td>
-                                        <td>Scumbag</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Darth Vader</td>
-                                        <td>Dark</td>
-                                        <td>Sith</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-
-                                <p className="py-2"><a href="#">See More issues...</a></p>
-
-                            </div>
-                        </div>
-                    </div>
-
                 <div className="w-full md:w-1/2 xl:w-1/3 p-6">
                     <div className="bg-white border-transparent rounded-lg shadow-xl">
                         <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-2">
